a1da71e878f4f98d4b95114de759aeccb753c00f
hun <96307763+hundotio@users.noreply.github.com>
2025-03-21 20:43:14 -0500

    dakotaconquest user stuff

---
+++ users.ps1
@@ -0,0 +1,28 @@
+Write-Host "`nStarting Windows User Hardening!`n"
+
+function Main {
+    Get-LocalUser | ForEach-Object { HardenUser -Username $_.Name }
+}
+
+function HardenUser {
+    param (
+        [string]$username
+    )    
+    $newadminpass = GenerateStrongPassword
+    Write-Host "$username's New Password: $newadminpass"
+    net user $user $newadminpass
+}
+
+function GenerateStrongPassword {
+    $length = 64
+
+    $charSet = @(
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'abcdefghijklmnopqrstuvwxyz',
+        '0123456789'
+    ) -join ''
+
+    -join (1..$length | ForEach-Object { $charSet[(Get-Random -Maximum $charSet.Length)] })
+}
+
+Main
